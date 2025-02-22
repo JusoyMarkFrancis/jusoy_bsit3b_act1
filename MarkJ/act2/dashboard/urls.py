7f51033904b8ad3c4aacbd71686da85e7dc19c42
JusoyMarkFrancis <157660711+JusoyMarkFrancis@users.noreply.github.com>
2025-02-22 23:16:25 +0800

    Delete MarkJ/act2 directory

--- MarkJ/act2/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import dashboard, reports, settings
-
-app_name = "dashboard"
-urlpatterns = [
-    path('', dashboard, name='index'),
-    path('reports/', reports, name='reports'),
-    path('settings/', settings, name='settings'),
-]
